b0f69f8a6bd618c0c6ce4f27b5c3f86fbccecbac
Mohammad Hashim <hashimmohammad1375@gmail.com>
2025-05-05 16:55:27 +0530

    Updated Add-On

--- Add-On/Array+Control-Statements/Sorting/MergeSort.java
+++ Add-On/Array+Control-Statements/Sorting/MergeSort.java
@@ -1,3 +1,89 @@
-public class MergeSort {
+import java.io.*;
+
+
+public class MergeSort 
+{
+    public static void merge(int[] a,int s,int e ,int m)
+    {
+        int i = s, j = m + 1,  k = 0;
+        int[] temp = new int[e-s+1];
+
+
+        while(i<=m && j<=e)
+        {
+            if(a[i] <= a[j])
+            {
+                temp[k++] = a[i++];
+            }
+            else
+            {
+                temp[k++] = a[j++];
+            }
+        }
+
+        while(i<=m)
+        {
+            temp[k++] = a[i++];
+        }
+
+        while(j<=e)
+        {
+            temp[k++] = a[j++];
+        }
+
+        for(k = 0; k < temp.length; k++) {
+            a[s + k] = temp[k];
+        }
+        
+    }
+
+    public static void msort(int[] a,int s,int e)
+    {
+        if(s<e)
+        {
+            int m = s + (e - s)/2;
+            msort(a,s,m);
+            msort(a,m+1,e);
+            MergeSort.merge(a,s,e,m);
+        }
+    }
     
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while (true)
+        {
+
+            System.out.print("\nEnter the size of array: ");
+            int n = Integer.parseInt(br.readLine());
+            System.out.print("Enter the elements: ");
+            int[] a = new int[n];
+            String[] inp = br.readLine().split(" ");
+            for(int i=0;i<n;i++)
+            {
+                a[i] = Integer.parseInt(inp[i]);
+            }
+            System.out.print("Array Loaded: ");
+        
+            for(int i=0;i<n;i++)
+            {
+                System.out.print(a[i] + " ");
+            }
+        
+            msort(a,0,n-1);
+            System.out.print("\nSorted Array: ");
+        
+            for(int i=0;i<n;i++)
+            {
+                System.out.print(a[i] + " ");
+            }
+
+            System.out.print("\nDo you want to continue? (yes/no): ");
+            String choice = br.readLine();
+            if (choice.equals("no") || choice.equals("No")) 
+            {
+                break;
+            }
+        }
+    }
 }
--- Add-On/Array+Control-Statements/Sorting/QuickSort.java
+++ Add-On/Array+Control-Statements/Sorting/QuickSort.java
@@ -1,3 +1,77 @@
-public class QuickSort {
+import java.io.*;
+
+
+public class QuickSort 
+{
+    public static void swap(int[] a,int x,int y)
+    {
+        int temp = a[x];
+        a[x] = a[y];
+        a[y] = temp;
+    }
+
+    public static int partition(int[] a,int s,int e)
+    {
+        int pivot = a[e];
+        int i = s - 1;
+        for(int j=s;j<e;j++)
+        {
+            if(a[j] < pivot)
+            {
+                i++;
+                swap(a,i,j);
+            }
+        }
+        swap(a,i+1,e);
+        return i+1;
+    }
+
+    public static void qsort(int[] a,int s,int e)
+    {
+        if(s<e)
+        {
+            int k = partition(a, s, e);
+            qsort(a,s,k-1);
+            qsort(a,k+1,e);
+        }
+    }
     
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while (true)
+        {
+
+            System.out.print("\nEnter the size of array: ");
+            int n = Integer.parseInt(br.readLine());
+            System.out.print("Enter the elements: ");
+            int[] a = new int[n];
+            String[] inp = br.readLine().split(" ");
+            for(int i=0;i<n;i++)
+            {
+                a[i] = Integer.parseInt(inp[i]);
+            }
+            System.out.print("Array Loaded: ");
+        
+            for(int i=0;i<n;i++)
+            {
+                System.out.print(a[i] + " ");
+            }
+        
+            qsort(a,0,n-1);
+            System.out.print("\nSorted Array: ");
+        
+            for(int i=0;i<n;i++)
+            {
+                System.out.print(a[i] + " ");
+            }
+
+            System.out.print("\nDo you want to continue? (yes/no): ");
+            String choice = br.readLine();
+            if (choice.equals("no") || choice.equals("No")) 
+            {
+                break;
+            }
+        }
+    }
 }
