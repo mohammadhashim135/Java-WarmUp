357653c2e8997be58681e6be17fc7a345b74a0a9
Mohammad Hashim <hashimmohammad1375@gmail.com>
2025-05-20 07:16:02 +0530

    Fianl key

---
+++ Quick-Recap/Final/FinalKey.java
@@ -0,0 +1,35 @@
+/*
+ FinalKey.java:9: error: cannot inherit from final A
+class B extends A 
+                ^
+1 error
+*/
+
+
+
+final class A
+{
+    void disp()
+    {
+        System.out.print("\nmohammadhashim.exe");
+    }
+}
+
+class B extends A 
+{
+    void show()
+    {
+        System.out.print("\nOye");
+    }
+}
+
+
+public class FinalKey 
+{
+    public static void main(String[] args)
+    {
+        B o = new B();
+        o.show();
+        o.disp();
+    }
+}
